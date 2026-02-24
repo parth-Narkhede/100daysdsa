be589cc7b2bdb297276f0cf5f8ccf92d986b3e0c
ParthNarkhede <theniceone6699@gmail.com>
2026-02-24 18:01:34 +0530

    Add files via upload

---
+++ question24.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertEnd(struct Node* head, int value) {
+    struct Node* newNode = createNode(value);
+    
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+struct Node* deleteFirstOccurrence(struct Node* head, int key) {
+    if (head == NULL)
+        return head;
+
+    struct Node* temp = head;
+    struct Node* prev = NULL;
+
+    if (temp->data == key) {
+        head = temp->next;
+        free(temp);
+        return head;
+    }
+
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+        return head;
+
+    prev->next = temp->next;
+    free(temp);
+
+    return head;
+}
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, value, key;
+    struct Node* head = NULL;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        head = insertEnd(head, value);
+    }
+
+    printf("Enter key to delete: ");
+    scanf("%d", &key);
+
+    printf("Original List: ");
+    printList(head);
+
+    head = deleteFirstOccurrence(head, key);
+
+    printf("\nList after deleting first occurrence of %d: ", key);
+    printList(head);
+     
+    printf("\n");
+
+    return 0;
+}

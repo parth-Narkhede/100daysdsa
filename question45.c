dea33f204f31eae26121f1b60897cf3e46bb2533
ParthNarkhede <theniceone6699@gmail.com>
2026-03-17 12:47:20 +0530

    Add files via upload

---
+++ question45.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+    int data;
+    struct Node *left;
+    struct Node *right;
+}Node;
+
+Node* createNode(int val){
+    Node* newnode = (Node*)malloc(sizeof(Node));
+    newnode->data = val;
+    newnode->left = NULL;
+    newnode->right = NULL;
+    return newnode;
+}
+
+int height(Node* root){
+    if(root == NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return (left > right ? left : right) + 1;
+}
+
+int main(){
+
+    int n;
+
+    printf("Enter number of nodes: ");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    printf("Enter level order traversal (-1 for NULL):\n");
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    Node* nodes[n];
+
+    for(int i=0;i<n;i++){
+        if(arr[i] == -1)
+            nodes[i] = NULL;
+        else
+            nodes[i] = createNode(arr[i]);
+    }
+
+    for(int i=0;i<n;i++){
+        if(nodes[i] != NULL){
+            int left = 2*i + 1;
+            int right = 2*i + 2;
+
+            if(left < n)
+                nodes[i]->left = nodes[left];
+
+            if(right < n)
+                nodes[i]->right = nodes[right];
+        }
+    }
+
+    Node* root = nodes[0];
+
+    printf("Height of tree: %d\n", height(root));
+
+    return 0;
+}

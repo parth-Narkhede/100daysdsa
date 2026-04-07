1073c9d879d55a899f403aa2ec78c9eaf1e48c5f
ParthNarkhede <theniceone6699@gmail.com>
2026-04-07 17:20:52 +0530

    Add files via upload

---
+++ question66.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int graph[MAX][MAX];
+int visited[MAX];
+int recStack[MAX];
+
+int dfs(int node, int n){
+
+    visited[node] = 1;
+    recStack[node] = 1;
+
+    for(int i = 0; i < n; i++){
+
+        if(graph[node][i]){
+
+            if(!visited[i]){
+                if(dfs(i, n))
+                    return 1;
+            }
+            else if(recStack[i]){
+                return 1;
+            }
+        }
+    }
+
+    recStack[node] = 0;
+    return 0;
+}
+
+int hasCycle(int n){
+
+    for(int i = 0; i < n; i++){
+        if(!visited[i]){
+            if(dfs(i, n))
+                return 1;
+        }
+    }
+
+    return 0;
+}
+
+int main(){
+
+    int n, e;
+    printf("Enter number of vertices: ");
+    scanf("%d",&n);
+
+    printf("Enter number of edges: ");
+    scanf("%d",&e);
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++)
+            graph[i][j] = 0;
+        visited[i] = 0;
+        recStack[i] = 0;
+    }
+
+    printf("Enter directed edges (u v):\n");
+    for(int i=0;i<e;i++){
+        int u, v;
+        scanf("%d %d",&u,&v);
+        graph[u][v] = 1;
+    }
+
+    if(hasCycle(n))
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
